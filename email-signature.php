8aa9132dd3f388c138731efdf8e1eaacd4070e02
Vaishali Agola <vaishali.agola@rtcamp.com>
2016-02-25 11:53:59 +0530

    class initialize and folder structure added

--- email-signature.php
+++ email-signature.php
@@ -17,3 +17,15 @@
 
 namespace rtCamp\WP\rtEmailSignature;
 
+//define constant for plugin directory path
+define( 'rtCamp\WP\rtEmailSignature\PATH', plugin_dir_path( __FILE__ ) );
+
+//define constant for plugin directory url
+define( 'rtCamp\WP\rtEmailSignature\URL', plugin_dir_url( __FILE__ ) );
+
+//include classes
+require_once \rtCamp\WP\rtRestaurants\PATH . 'includes/class-email_signature.php';
+
+//instanciate class Email_Signature and call init()
+$email_signature = new Email_Signature();
+$email_signature->init();
