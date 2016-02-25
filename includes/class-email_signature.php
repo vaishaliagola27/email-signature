7c9d61c6a55b5610177762e16ad44175a0631ae1
Vaishali Agola <vaishali.agola@rtcamp.com>
2016-02-25 11:54:27 +0530

    class added

---
+++ includes/class-email_signature.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace rtCamp\WP\rtEmailSignature;
+
+if ( !class_exists( 'Email_Signature' ) ) {
+
+	/**
+	 * Email signature
+	 *
+	 * @author Vaishali Agola <vaishaliagola27@gmail.com>
+	 */
+	class Email_Signature {
+		public function init(){
+			
+		}
+	}
+
+}
