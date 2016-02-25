37d3aae38d11992a489d4244ecd86855ac876a6a
Vaishali Agola <vaishali.agola@rtcamp.com>
2016-02-25 11:41:40 +0530

    initalize plugin

---
+++ email-signature.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+  Plugin Name: rtEmailSignature
+  Plugin URI:  http://rtcamp.com
+  Description: Restaurat directory
+  Version:     0.1
+  Author:      vaishuagola27
+  Author URI:  http://rtcamp.com
+  License:     GPL2
+  License URI: https://www.gnu.org/licenses/gpl-2.0.html
+  Domain Path: /languages
+  Text Domain: rt-restaurants
+ */
+// Custom code starts here
+//namespace declaration
+
+namespace rtCamp\WP\rtEmailSignature;
+
